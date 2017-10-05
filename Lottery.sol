c3bc521bfe2d7a7833e3e4425a14ae90f1b095aa
Michael Garber <posterfearguy@gmail.com>
2017-10-05 14:19:40 -0400

    calculating winnings and subtracting charity fee

--- Lottery.sol
+++ Lottery.sol
@@ -8,7 +8,10 @@ contract USDOracle {
 contract Lottery{
     
     address owner;
-    uint public creationTime = now;
+    uint private numPlayers = 0;
+    uint creationTime = now;
+    uint jackPot = 0;
+    uint charityFee;
     
     USDOracle oracle = USDOracle(0x1c68f4f35ac5239650333d291e6ce7f841149937);
     uint constant USD_IN_CENTS = 100;
@@ -22,6 +25,7 @@ contract Lottery{
         uint numberGuessed;
     }
     
+    
     function Lottery () payable {
         owner = msg.sender;
     }
@@ -31,6 +35,15 @@ contract Lottery{
             throw;
         _;
     }
+    
+    modifier endPlay {
+        if (creationTime + now == 7 days){
+            _;
+        }
+        throw;
+    }
+
+    event Log(string message, address caller);
 
     function currentRate() constant returns (uint ratePerDollar) {
         ratePerDollar = USD_IN_CENTS * oracle.WEI();
@@ -38,7 +51,7 @@ contract Lottery{
     }
     
     function buyTicket (uint _numberGuessed,
-    string _firstName, string _lastName) payable returns (string) {
+    string _firstName, string _lastName) payable {
         
         
         //if (msg.value != USD_IN_CENTS * oracle.WEI()) throw;
@@ -48,15 +61,56 @@ contract Lottery{
         entry.firstName = _firstName;
         entry.lastName = _lastName;
         entry.numberGuessed = _numberGuessed;
+        numPlayers++;
+        jackPot += msg.value;
         
-        
-        return ("You have entered into the lottery");
+        Log("You have entered in to the lottery", msg.sender);
         
     }
     
     function getTicket () constant returns (string, string, uint) {
         var entry = entries[msg.sender];
         return (entry.firstName, entry.lastName, entry.numberGuessed);
+    }
+    
+    /// Pick a winner for small lotteries put winnings in entry's address
+    function pickSmallWinner () ifOwner endPlay {
+        //Oracilize a "random" number
+        if (numPlayers < 100){
+            // pick the winner
+            uint randomNum = 0;
+            uint winner = randomNum % numPlayers; // Gets the index of the winner
+            
+            // subtract 2% charity fee from jackPot
+            charityFee = (jackPot * 2) / 100;
+            jackPot -= charityFee;
+            //entries[winner].transfer(jackPot);
+            
+        }
+        
+    }
+    
+    /// Pick a winner for a big lottery (over 100 participants) put earnings into winners address
+    function pickBigWinner () ifOwner endPlay {
+        // Oraclize a "random" number
+        uint randomNum = 0; //change this to the random function
+        // For small lotteries, we will just pick a random player
+        
+        
+        if (numPlayers > 100){
+            // pick the winner
+            
+            
+            // subtract 2% charity fee from jackPot
+            charityFee = (jackPot * 2) / 100;
+            jackPot -= charityFee;
+    
+        }        
+    }
+    
+    /// Allow winner to withrawl their earnings
+    function withdrawlWinnings () {
+        
     }
     
     
