5f8e57f06a88500c55435ead2829d7840cb0a56b
Michael Garber <posterfearguy@gmail.com>
2017-10-07 13:28:01 -0400

    Updated pickWinner for different size lotteries

--- Lottery.sol
+++ Lottery.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.11;
 
 /// This is so we can set the price in USD constants
 contract USDOracle {
@@ -7,8 +7,9 @@ contract USDOracle {
 }
 contract Lottery{
     
-    address owner;
-    uint private numPlayers = 0;
+    address private owner;
+    address[] participants;
+    uint[] numbersGuessed;
     uint creationTime = now;
     uint jackPot = 0;
     uint charityFee;
@@ -16,17 +17,7 @@ contract Lottery{
     USDOracle oracle = USDOracle(0x1c68f4f35ac5239650333d291e6ce7f841149937);
     uint constant USD_IN_CENTS = 100;
     
-    
-    mapping (address => Entry) entries;
-    
-    struct Entry {
-        string firstName;
-        string lastName;
-        uint numberGuessed;
-    }
-    
-    
-    function Lottery () payable {
+    function Lottery () {
         owner = msg.sender;
     }
     
@@ -50,32 +41,29 @@ contract Lottery{
         return ratePerDollar;
     }
     
-    function buyTicket (uint _numberGuessed,
-    string _firstName, string _lastName) payable {
+    function buyTicket (uint _numberGuessed) payable {
         
         
         //if (msg.value != USD_IN_CENTS * oracle.WEI()) throw;
+        if ( msg.value != 1 ether ) throw;
         
-        var entry = entries[msg.sender];
-        
-        entry.firstName = _firstName;
-        entry.lastName = _lastName;
-        entry.numberGuessed = _numberGuessed;
-        numPlayers++;
         jackPot += msg.value;
+        participants.push(msg.sender);
+        numbersGuessed.push(_numberGuessed);
         
         Log("You have entered in to the lottery", msg.sender);
         
     }
     
-    function getTicket () constant returns (string, string, uint) {
-        var entry = entries[msg.sender];
-        return (entry.firstName, entry.lastName, entry.numberGuessed);
-    }
+    //function getTicket () constant returns (string, string, uint) {
+        //var entry = entries[msg.sender];
+       // return (entry.firstName, entry.lastName, entry.numberGuessed);
+    //}
     
     /// Pick a winner for small lotteries put winnings in entry's address
     function pickSmallWinner () ifOwner endPlay {
         //Oracilize a "random" number
+        uint numPlayers = participants.length;
         if (numPlayers < 100){
             // pick the winner
             uint randomNum = 0;
@@ -84,7 +72,7 @@ contract Lottery{
             // subtract 2% charity fee from jackPot
             charityFee = (jackPot * 2) / 100;
             jackPot -= charityFee;
-            //entries[winner].transfer(jackPot);
+            participants[winner].transfer(jackPot);
             
         }
         
@@ -95,16 +83,20 @@ contract Lottery{
         // Oraclize a "random" number
         uint randomNum = 0; //change this to the random function
         // For small lotteries, we will just pick a random player
+        uint numPlayers = participants.length;
         
         
         if (numPlayers > 100){
             // pick the winner
-            
-            
-            // subtract 2% charity fee from jackPot
-            charityFee = (jackPot * 2) / 100;
-            jackPot -= charityFee;
-    
+            // check for match
+            for (uint i = 0; i < numPlayers; i++) {
+                if (numbersGuessed[i] == randomNum) {
+                    // subtract 2% charity fee from jackPot
+                    charityFee = (jackPot * 2) / 100;
+                    jackPot -= charityFee;
+                    participants[i].transfer(jackPot);
+                }
+            }
         }        
     }
     
@@ -112,6 +104,4 @@ contract Lottery{
     function withdrawlWinnings () {
         
     }
-    
-    
 }
